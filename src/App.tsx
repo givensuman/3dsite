77106418c34352279c73db3863d6803b7477c7ef
givensuman <givensuman@gmail.com>
2024-01-30 19:06:59 -0500

    Start dev on new designs

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
-import About from "./pages/About"
-import Home from "./pages/Home"
-import Contact from "./pages/Contact"
-import Projects from "./pages/Projects"
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { AnimatePresence } from "framer-motion"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/projects",
-    element: <Projects />
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  }
-])
+import React from "react"
+import { PinContainer } from "./components/3d-pin"
+import Title from "./components/Title"
 
 function App() {
   return (
     <>
-      <AnimatePresence>
-        <RouterProvider router={router} />
-      </AnimatePresence>
+      <Title />
+      <div className="h-[40rem] w-full flex items-center justify-center ">
+        <PinContainer
+          title="/ui.aceternity.com"
+          href="https://twitter.com/mannupaaji"
+        >
+          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+            <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+              Aceternity UI
+            </h3>
+            <div className="text-base !m-0 !p-0 font-normal">
+              <span className="text-slate-500 ">
+                Customizable Tailwind CSS and Framer Motion Components.
+              </span>
+            </div>
+            <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+          </div>
+        </PinContainer>
+      </div>
     </>
   )
 }
---
+++ src/components/Title.tsx
@@ -0,0 +1,9 @@
+const Title = () => {
+  return (
+    <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+      Given Suman
+    </h1>
+  )
+}
+
+export default Title
